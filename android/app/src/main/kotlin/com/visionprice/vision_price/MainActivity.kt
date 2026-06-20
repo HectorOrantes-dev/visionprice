403134172909709ab812d5f9d33e59b95f7d9c67
joacogomezz <233360@ids.upchiapas.edu.mx>
2026-06-20 16:31:22 -0600

    update

--- android/app/src/main/kotlin/com/visionprice/vision_price/MainActivity.kt
+++ android/app/src/main/kotlin/com/visionprice/vision_price/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.visionprice.vision_price
 
+import android.content.Intent
+import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val raspChannel = "com.visionprice.vision_price/rasp"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, raspChannel)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "openDeveloperSettings") {
+                    startActivity(Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS))
+                    result.success(true)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+}
