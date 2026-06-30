998bba7e254dd09bcb4ef97f61bafa6c028778d2
joacogomezz <233360@ids.upchiapas.edu.mx>
2026-06-30 17:46:56 -0600

    update

--- android/app/src/main/kotlin/com/visionprice/vision_price/MainActivity.kt
+++ android/app/src/main/kotlin/com/visionprice/vision_price/MainActivity.kt
@@ -2,11 +2,11 @@ package com.visionprice.vision_price
 
 import android.content.Intent
 import android.provider.Settings
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val raspChannel = "com.visionprice.vision_price/rasp"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
